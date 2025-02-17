b898655f5d73766a48b52ff3d792394b2c4c51c1
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-02-18 01:43:42 +0600

    NavBar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:'#000957',
+        secondary:'#FFEB00'
+      }
+    },
   },
+  darkMode:'class',
+  mode:'jit',
   plugins: [],
 }
 
